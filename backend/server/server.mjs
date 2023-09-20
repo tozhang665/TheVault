6ae8c9f9723c4dbf6a7d845617590dd78f0d362b
Tony Zhang <tozhang665@gmail.com>
2023-09-19 18:15:48 -0700

    hopefully this will give us __dirname

--- backend/server/server.mjs
+++ backend/server/server.mjs
@@ -2,6 +2,11 @@ import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import records from "./routes/record.mjs";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 import * as path from 'path';
 const PORT = process.env.PORT || 5050;
