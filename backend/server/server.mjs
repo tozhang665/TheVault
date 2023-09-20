b7ac2e9874198f68b86c5e18c8c69a23ca3a18f7
Tony Zhang <tozhang665@gmail.com>
2023-09-19 17:36:02 -0700

    setting up server for heroku

--- backend/server/server.mjs
+++ backend/server/server.mjs
@@ -3,6 +3,7 @@ import cors from "cors";
 import "./loadEnvironment.mjs";
 import records from "./routes/record.mjs";
 
+const path = require("path")
 const PORT = process.env.PORT || 5050;
 const app = express();
 
@@ -10,6 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/record", records);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static("client/build"))
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "client","build", "index.html"))
+  })
+}
 
 // start the Express server
 app.listen(PORT, () => {
