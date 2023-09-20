16ef26123887c20956960d8181fa0b6d07bec4c1
Tony Zhang <tozhang665@gmail.com>
2023-09-19 21:25:18 -0700

    gosh darn it

--- server.mjs
+++ server.mjs
@@ -18,9 +18,9 @@ app.use(express.json());
 app.use("/record", records);
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static("client/build"))
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname, "frontend","build", "index.html"))
+  app.use(express.static("frontend/build"));
+  app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend","build", "index.html"));
   })
 }
 
