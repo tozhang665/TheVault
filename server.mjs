dc7991e5ea18d5303c26a890af62e75c36872a35
Tony Zhang <tozhang665@gmail.com>
2023-09-21 18:47:23 -0700

    maybe this will fix it who knows

--- server.mjs
+++ server.mjs
@@ -14,7 +14,7 @@ const app = express();
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static("frontend/build"));
-  app.get("/",(req,res)=>{
+  app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname, "frontend","build", "index.html"));
   })
 }
